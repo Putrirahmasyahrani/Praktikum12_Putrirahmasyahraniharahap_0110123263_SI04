7e9a7aa05cbaa9e08312848e18e4dd73cb0feba7
Putrirahmasyahrani <146744194+Putrirahmasyahrani@users.noreply.github.com>
2023-12-27 19:51:21 +0700

    Add files via upload

---
+++ Praktikum12/Otentikasi.js
@@ -0,0 +1,19 @@
+const username = "yani"
+const password = "123456"
+
+function auth(){
+    // tangkap nilai yg diinput user
+    let userInput = document.getElementById("username").value
+    let passwordInput = document.getElementById("password").value
+
+    let form = document.getElementById("form")
+
+    if(username == userInput && password ==  passwordInput){
+        alert("Login Berhasil")
+        form.submit()
+    } 
+    else{
+        alert("Login Gagal!")
+        
+    }
+}
